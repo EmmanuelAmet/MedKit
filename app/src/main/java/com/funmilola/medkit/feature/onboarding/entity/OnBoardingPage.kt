01c3c8d874dc7d2edb7c367c636a3a8f5340e197
Adeoluwa <apata.adeoluwa@gmail.com>
2020-10-17 16:24:10 +0100

    Splash screen and the onboarding screens

---
+++ app/src/main/java/com/funmilola/medkit/feature/onboarding/entity/OnBoardingPage.kt
@@ -0,0 +1,18 @@
+package com.funmilola.medkit.feature.onboarding.entity
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.funmilola.medkit.R
+
+enum class OnBoardingPage(
+    @StringRes val titleResource: Int,
+    @StringRes val subTitleResource: Int,
+    @StringRes val descriptionResource: Int,
+    @StringRes val headline: Int,
+    @DrawableRes val logoResource: Int
+) {
+
+    ONE(R.string.onboarding_slide1_title, R.string.onboarding_slide1_subtitle,R.string.onboarding_slide1_desc,R.string.onboarding_slide1_headline, R.drawable.scanscreen),
+    TWO(R.string.onboarding_slide2_title, R.string.onboarding_slide2_subtitle,R.string.onboarding_slide2_desc,R.string.onboarding_slide2_headline, R.drawable.meetadoc),
+    //THREE(R.string.onboarding_slide2_title, R.string.onboarding_slide3_subtitle,R.string.onboarding_slide1_desc,R.string.onboarding_slide2_headline, R.drawable.ic_a_day_at_the_park)
+
+}
