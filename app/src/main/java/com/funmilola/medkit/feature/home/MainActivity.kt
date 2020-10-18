bbab7ac30aceb5eeb0aeda01c1bc9aa4b52beeb5
Adeoluwa <apata.adeoluwa@gmail.com>
2020-10-18 19:26:19 +0100

    Splash screen, onboarding screen linked with the login screen

---
+++ app/src/main/java/com/funmilola/medkit/feature/home/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.funmilola.medkit.feature.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.funmilola.medkit.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
