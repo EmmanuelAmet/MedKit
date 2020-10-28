1a1b3aef312f7801bd69f7a4380e3ef8825b4221
Ademola John <AdemolaJhon@gmail.com>
2020-10-29 00:17:57 +0100

    added drugs identified page, user profile page

---
+++ app/src/main/java/com/funmilola/medkit/feature/user_profile/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.funmilola.medkit.feature.user_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.funmilola.medkit.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
